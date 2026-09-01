ff2a0d5958ee7e9ad26a11dad21baeefebb1d5df
Nelson Ramírez <xhyskoax@gmail.com>
2026-08-31 20:01:13 -0400

    se agrega endpoint y controlador para obtener usuarios por id.

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -2,13 +2,11 @@ import Usuario from "../models/Usuario.model.js";
 
 export const getAllUsuarios = async (req, res) => {
     try {
-
         const usuarios = await Usuario.findAll({
-            attributes: ["id", "nombre", "email"]
+            attributes: ["id", "nombre", "email"],
         });
 
-        res.json({status: "Ok", usuarios});
-        
+        res.json({ status: "Ok", usuarios });
     } catch (error) {
         console.error(error);
         res.status(500).json({
@@ -16,4 +14,31 @@ export const getAllUsuarios = async (req, res) => {
             message: "Error interno del servidor.",
         });
     }
-}
+};
+
+export const getUsuariosById = async (req, res) => {
+    try {
+        let { id } = req.params;
+
+        const usuario = await Usuario.findByPk(id, {
+            attributes: ["id", "nombre", "email"],
+        });
+
+        if (!usuario) {
+            return res
+                .status(404)
+                .json({
+                    status: "Not found",
+                    message: "No se encontró ningún usuario con el ID. " + id,
+                });
+        }
+
+        res.json({ status: "Ok", usuario });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: "error",
+            message: "Error interno del servidor.",
+        });
+    }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -7,6 +7,9 @@ const router = express.Router();
 //RUTA OBTENER TODOS LOS USUARIOS
 router.get("/", usuariosControllers.getAllUsuarios);
 
+//RUTA OBTENER USUARIOS POR SU ID
+router.get("/:id", usuariosControllers.getUsuariosById);
+
 
 
 export default router;
