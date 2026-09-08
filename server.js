b69a535111bfc0cfe9f3fb4f1e4e45499368c7a7
Nelson Ramírez <xhyskoax@gmail.com>
2026-09-08 20:16:41 -0300

    Se cambia ordern importación asociaciones

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import app from "./src/app.js";
-import sequelize from "./src/config/database.js";
 import "./src/models/index.js";
+import sequelize from "./src/config/database.js";
+
 
 const PORT = 3000;
 
