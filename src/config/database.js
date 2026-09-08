b2537bd8436fa7eca777e5b9fa0fe4f3b0902ff7
Nelson Ramírez <xhyskoax@gmail.com>
2026-09-08 19:48:37 -0300

    Se prepara app para deploy en vercel

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,17 @@
 import { Sequelize } from "sequelize";
+import pg from "pg";
 
 //motor_db://usuario:password@direccion_host:puerto/nombre_db
 
 const URI_DATABASE = process.env.URI_DATABASE;
 
-const sequelize = new Sequelize(URI_DATABASE);
+const sequelize = new Sequelize(URI_DATABASE, {
+    dialect: "postgres",
+    dialectModule: pg,
+    dialectOptions:
+        process.env.ENV_NODE == "production"
+            ? { require: true, ssl: { rejectUnauthorized: false } }
+            : {},
+});
 
 export default sequelize;
